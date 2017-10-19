bbdf8b893996b35299af88b498c622d9d339cc02
1moregame <jacob.d.davis@gmail.com>
2017-10-19 13:35:34 -0500

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     # @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
-    @orders = [1009, 1006].collect {|order_id| Shopify::Order.find(order_id)}
+    @orders = [1009, 1006].collect {|order_id| ShopifyAPI::Order.find(order_id)}
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
