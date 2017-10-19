344b76650bef0f50d5291706493609acae2ba42f
1moregame <jacob.d.davis@gmail.com>
2017-10-19 13:22:00 -0500

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
+    # @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
+    @orders ShopifyAPI::Order.find(:all, :params => { :ids => "1009,1006" })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
 end
