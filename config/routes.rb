98099b88055d62137dfe481f79ccb9d3ef430ad8
1moregame <jacob.d.davis@gmail.com>
2017-10-20 14:33:25 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :subscriptions, :only => [:create]
+  resources :home, :only => [:create]
 
 
 end
