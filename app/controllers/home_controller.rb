92635a3fb9dace071aab429ea7c66af266a998f5
1moregame <jacob.d.davis@gmail.com>
2017-10-20 10:59:49 -0500

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     #@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @products = ShopifyAPI::Product.where(product_type: "Tamale")or(ShopifyAPI::Product.where(product_type: "Gift Packages"))
+    @products = ShopifyAPI::Product.where(product_type: "Tamale").or(ShopifyAPI::Product.where(product_type: "Gift Packages"))
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
