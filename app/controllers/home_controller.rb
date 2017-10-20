57b6c756c57953f08d998be9fb0a7f65264df7f0
1moregame <jacob.d.davis@gmail.com>
2017-10-20 13:26:05 -0500

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,8 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    #@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @products = ShopifyAPI::Product.where(product_type: "Tamale").or(ShopifyAPI::Product.where(product_type: "Gift Packages"))
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
-  def order_metafield
-    @order = ShopifyAPI::Order.find(params[:id])
-    order.add_metafield(ShopifyAPI::Metafield.new( {
-        :namespace => 'packing_list',
-        :key => 'time_date',
-        :value => "100",
-        :value_type => 'integer'
-        })) %>
-
-  redirect_to index
-  end
+  
 end
