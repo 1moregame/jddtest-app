72a0f730e9517810efee4799b1c8a0250ccddc0a
1moregame <jacob.d.davis@gmail.com>
2017-10-20 13:39:56 -0500

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,16 @@ class HomeController < ShopifyApp::AuthenticatedController
     @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
-  
+  def create
+    @order = ShopifyAPI::Order.find(params[:id])
+    order.add_metafield(ShopifyAPI::Metafield.new( {
+        :namespace => 'packing_list',
+        :key => 'time_date',
+        :value => "100",
+        :value_type => 'integer'
+        })) %>
+
+    redirect_to @index 
+    flash[:notice] = "You're now subscribed to #{@order.name}! WooHoo!"
+  end
 end
